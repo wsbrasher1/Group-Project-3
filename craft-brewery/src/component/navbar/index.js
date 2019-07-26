c1f5f89f9b6c722843a80834b1fed52a4327176e
Cameron <cameronleecalder@gmail.com>
2019-07-25 20:31:20 -0500

    navbar test commit

---
+++ craft-brewery/src/component/navbar/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = props => (
+    <navbar bg="dark">
+        <Navbar.Brand href="#home">Craft Catcher</Navbar.Brand>
+        <Navbar.Text href="#events">Events</Navbar.Text>
+    </navbar>
+);
+
+export default Navbar;
