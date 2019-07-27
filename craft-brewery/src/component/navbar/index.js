d0325358ec643135dced1b49d49d833b21ebf1ae
Cameron <cameronleecalder@gmail.com>
2019-07-27 11:34:40 -0500

    201907271134 commit

--- craft-brewery/src/component/navbar/index.js
+++ craft-brewery/src/component/navbar/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 
-const Navbar = props => (
-    <navbar bg="dark">
-        <Navbar.Brand href="#home">Craft Catcher</Navbar.Brand>
-        <Navbar.Text href="#events">Events</Navbar.Text>
-    </navbar>
+const TopNav = props => (
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 );
 
-export default Navbar;
+export default TopNav;
