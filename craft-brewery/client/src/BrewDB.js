fc6d7ab5def98d5f20702a8a4f6a1977d2cf79ad
Bill Brasher <wsbrasher1@msn.com>
2019-07-30 21:12:22 -0500

    added dotenv

--- craft-brewery/client/src/BrewDB.js
+++ craft-brewery/client/src/BrewDB.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
 const API_KEY = process.env.REACT_APP_BREWERYDB_API_KEY;
-const urlForUsersearch = usersearch =>
-    `https://api.brewerydb.com/v2/${usersearch}/?key=${API_KEY}`;
+// const urlForUsersearch = usersearch =>
+//     `https://api.brewerydb.com/v2/${usersearch}/?key=${API_KEY}`;
 
 class BreweryDB extends Component {
     constructor(props) {
---
+++ craft-brewery/client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = (props) => {
+   return (
+    <div className="header">
+        <h2 className="title">Here I am, trick</h2>  
+    </div>
+ );
+}
+
+export default Header;
