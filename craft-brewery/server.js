a3748bb9d84df38be39abe8abd9933291af9a707
Bill Brasher <wsbrasher1@msn.com>
2019-07-31 19:05:21 -0500

    Added schema model and additional javascript. Also made changes to the server.js file.

--- craft-brewery/server.js
+++ craft-brewery/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./DB.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const dotenv = require('dotenv').config();
 
-
+//To set up mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected, holmes!') },
+  err => {console.log('Can not connect to database' + err)}
+);
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Check with Tyler on this - example showed using bodyParser instead of express - need to know what is correct//
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -28,5 +42,5 @@ app.get("*", (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server is running on port ${PORT}!`);
 });
